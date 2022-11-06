29f470943fc4bdb46d4e5a0203a164b4930ee794
Adriano Ferreira Matias <110870765+M4tias04@users.noreply.github.com>
2022-11-06 09:46:45 -0300

    Add files via upload

---
+++ ATV3/Class1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATV03
+{
+    internal class Aluno
+    {
+        private string nome;
+    private int matricula;
+    public string email;
+
+    public Aluno(string nome)
+    {
+        this.nome = nome;
+    }
+
+    public Aluno()
+    {
+    }
+
+    public Aluno(string nome, int matricula, string email)
+    {
+        this.nome = nome;
+        this.matricula = matricula;
+        this.email = email;
+    }
+
+
+    // get e set nome
+    public string getNome()
+    {
+        return this.nome;
+    }
+
+    public void setNome(string nome)
+    {
+        this.nome = nome;
+    }
+
+    // get e set matricula
+    public int getMatricula()
+    {
+        return this.matricula;
+    }
+
+    public void setMatricula(int matricula)
+    {
+        this.matricula = matricula;
+    }
+
+    // get e set email
+    public string getEmail()
+    {
+        return this.email;
+    }
+
+    public void setEmail(string email)
+    {
+        this.nome = email;
+    }
+
+    public void imprimir()
+    {
+        Console.WriteLine(this.nome + " " + this.matricula + " " + this.email);
+    }
+
+
+}
+
+
+
+
---
+++ ATV3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATV03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Aluno a1 = new Aluno();
+            a1.setNome("Zé");
+            a1.setMatricula(234);
+            a1.setEmail("ze@gmail.com");
+
+            Aluno a2 = new Aluno("Ana");
+            a2.setMatricula(789);
+            a2.setEmail("ana@xy.com");
+
+            Aluno a3 = new Aluno("Paulo", 989, "paulo@ig.com");
+
+            a1.imprimir();
+            a2.imprimir();
+            a3.imprimir();
+
+            Console.ReadKey();
+        }
+    }
+}
+
